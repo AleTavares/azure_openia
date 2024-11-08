8ddf42826773f5fb3c70451dd930002eb7e93468
aletavares <portalatibaia@gmail.com>
2024-11-08 07:39:19 -0300

    Infra e Notebooks de configurações

--- azure_ia_service.tf
+++ azure_ia_service.tf
@@ -21,3 +21,17 @@ resource "azurerm_cognitive_account" "text_openia" {
   sku_name            = var.skuOpenIA
   kind                = var.kindOpenAI
 }
+
+
+# Cognitive Deployments 
+resource "azurerm_cognitive_deployment" "gpt-4o-mini" {
+  name                 = "gpt-4o-mini"
+  cognitive_account_id = azurerm_cognitive_account.text_openia.id
+  model {
+    format = "OpenAI"
+    name   = "gpt-4o-mini"
+  }
+  sku {
+    name = "Standard"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,30 @@
-# output "storage_account_id" {
-#   description = "ID da Storage Account Criada no Azure"
-#   value       = azurerm_storage_account.storage_acount.id
-# }
+output "end_point_translator" {
+  description = "End Point do Serviço de Translator do Azure Criado"
+  value       = nonsensitive(azurerm_cognitive_account.text_tranlator.endpoint)
+}
 
-# output "sa_primary_access_key" {
-#   description = "Primary Access Key da Storage Account Criada no Azure"
-#   value       = azurerm_storage_account.storage_acount.primary_access_key
-#   sensitive   = true
-# }
+output "subscription_key_translator" {
+  description = "End Point do Serviço de Translator do Azure Criado"
+  value       = nonsensitive(azurerm_cognitive_account.text_tranlator.primary_access_key)
+}
+
+output "end_point_open_ia" {
+  description = "End Point do Serviço da OpenIA criado no Azure"
+  value       = nonsensitive(azurerm_cognitive_account.text_openia.endpoint)
+}
+
+output "subscription_key_open_ia" {
+  description = "End Point do Serviço da OpenIA criado no Azure"
+  value       = nonsensitive(azurerm_cognitive_account.text_openia.primary_access_key)
+}
+
+output "version_api_open_ia" {
+  description = "versão da API da OpenIA criado no Azure"
+  value       = nonsensitive(azurerm_cognitive_deployment.gpt-4o-mini)
+}
+
+output "subscription_location_translator" {
+  description = "End Point do Serviço de Translator do Azure Criado"
+  value       = nonsensitive(azurerm_cognitive_account.text_tranlator.location)
+}
+
